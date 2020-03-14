1d552c47064b23fe3bb10e011d7629d2ac8ce116
casperOne <casperOne@caspershouse.com>
2020-03-14 18:47:16 -0400

    Added error and warning properties.

--- src/Ofl.Slack.Abstractions/WebApi/Response.cs
+++ src/Ofl.Slack.Abstractions/WebApi/Response.cs
@@ -7,5 +7,9 @@ namespace Ofl.Slack.WebApi
     public class Response
     {
         public bool Ok { get; set; }
+
+        public string? Error { get; set; }
+
+        public string? Warning { get; set; }
     }
 }
